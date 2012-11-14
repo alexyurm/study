3faa407e1c53f545f9e31572454a338625847472
alexyuisme <alexyuisme@gmail.com>
2012-11-13 22:37:35 -0800

    git commit -a

--- DataStructure/Chapter3_stack_and_queue/stack.c
+++ DataStructure/Chapter3_stack_and_queue/stack.c
@@ -1,21 +1,110 @@
 #include "stack.h"
 
-Status InitQueue(LinkQueue *queue_p)
+Status InitStack(SqStack *stack_p)
 {
-   queue_p->front = queue_p->rear = (QueuePtr)malloc(sizeof(QNode));
-   if (queue_p->front == NULL)
+   stack_p->base_p = (ElemType *)malloc(STACK_INIT_SIZE*sizeof(ElemType));
+
+   if (!(stack_p->base_p))
    {
-      printf("Failed to initialize a queue!\n");
-      return OVERFLOW;
+      printf("Not enough heap memory to construct a new stack\n");
+      return OVERFLOW;   
    }
    else
    {
-      queue_p->front->next = NULL;   
+      stack_p->stacksize = STACK_INIT_SIZE;
+      stack_p->top_p = stack_p->base_p;
       return OK;
    }
 }
 
-Status DestroyQueue(LinkQueue *queue_p)
+Status DestroyStack(SqStack *stack_p)
 {
-   while()
+   if ( stack_p->base_p != NULL )
+   {
+      free(stack_p->base_p);
+      stack_p->base_p = NULL;
+      stack_p->top_p = NULL;
+      stack_p->stacksize = 0;
+      stack_p = NULL;
+
+      return OK;
+   }
+   else
+   {
+      return INFEASIBLE;  
+   }
+}
+
+Status GetTop(SqStack stack, ElemType *elem_p)
+{
+   if (stack.base_p == stack.top_p)
+   {
+      return ERROR;
+   }
+   else
+   {
+      *elem_p = *(stack.top_p - 1);
+      return OK;
+   }
+}
+
+Status Push(SqStack *stack_p, ElemType elem)
+{
+
+   if ( stack_p->top_p - stack_p->base_p >= stack_p->stacksize )
+   {
+      /* Reallocate the memory */
+      stack_p->base_p = (ElemType *)realloc( stack_p->base_p, (stack_p->stacksize + STACKINCREMENT)*sizeof(ElemType) );
+      if (stack_p->base_p != NULL)
+      {
+         /* Successfully reallocate the memory */
+         stack_p->top_p = stack_p->base_p + stack_p->stacksize * sizeof(ElemType);
+         stack_p->stacksize = stack_p->stacksize + STACKINCREMENT;
+         
+         /* Push the element into the stack */
+         *(stack_p->top_p) = elem;
+         stack_p->top_p++;
+         
+         return OK;
+      }
+      else
+      {
+         printf("Not enough heap memory to construct a new stack\n");
+         return OVERFLOW;   
+      }
+   }
+   else
+   {
+      /* Push the element into the stack */
+      *(stack_p->top_p) = elem;
+      stack_p->top_p++;
+
+      return OK;
+   }
+}
+
+Status Pop(SqStack *stack_p, ElemType *elem_p )
+{
+   if ( stack_p->top_p == stack_p->base_p )
+   {
+      printf(" The stack is empty, nothing to pop,\n");
+      return INFEASIBLE;
+   }
+   else
+   {
+      *(elem_p) = *(--stack_p->top_p);
+      return OK;
+   }
+}
+
+Status StackEmpty(SqStack stack)
+{
+   if (stack.top_p == stack.base_p)
+   {
+      return TRUE;
+   }
+   else
+   {
+      return FALSE;
+   }
 }
