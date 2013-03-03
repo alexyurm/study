4666502a95aad8c1e927427711a6494e10f94d6b
alexyu <alexyu@alexyu.(none)>
2013-03-02 23:59:20 -0800

    git commit -a

---
+++ Java/TestMap/TestMap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class TestMap {
+   public static void main(String[] args) {
+      HashMap <String, Integer> scores = new HashMap<String, Integer>(); //<---HashMap needs TWO type parameters - one for the key and one for the value.
+
+      scores.put("Kathy", 42);
+      scores.put("Bert", 343);
+      scores.put("Skyler", 420);
+
+      System.out.println(scores);
+      System.out.println(scores.get("Bert"));
+   }
+} 
