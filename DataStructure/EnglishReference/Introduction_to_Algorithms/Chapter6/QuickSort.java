3a0aa94f1e8143d655162f064de8beafe8002632
alexyu <alexyu@alexyu-desktop.(none)>
2013-04-06 19:23:39 -0700

    git commit -a

--- DataStructure/EnglishReference/Introduction_to_Algorithms/Chapter6/QuickSort.java
+++ DataStructure/EnglishReference/Introduction_to_Algorithms/Chapter6/QuickSort.java
@@ -18,30 +18,109 @@ import java.lang.Integer;
 *
 *  Combine: Because the subarrays are already sorted, no work is needed to combine them: the entire array[p,..r] is now sorted.
 *
+*  -  At the beginning of each iteration of the loop in partition, for any array index k, 
+*     
+*     1. If p<=k<=i, then A[k]<=x
+*     2. If i+1<=k<=j-1, then A[k]>x
+*     3. If k=r, then A[k]=x
+*
+*  -  Worst case partition:   The worst case behavior for quicksort occurs when the partitioning routine produces one subproblem
+*                             with n-1 elements and one with 0 elements(why 0 element instead of 1 element??).
+*
+*
+*
+* -   Best-case partitioning: In the most even possible split, PARTITION produces two subproblems, each of size no more than n/2. 
+*                             each of size no more than n/2, since one is of size floor(n/2) and one of size ceil(n/2)-1.
+*
+*
+*
 */
 
+
 class QuickSort {
-   private int []A;
+   private int[] A;
    private int p;
-   private int q;
    private int r;
    
-   public void partition(int[] A, int p, int r) {
+   QuickSort() {
+      p =  r = 0;
+   }
+
+   QuickSort(int[] array, int lower, int upper) {
+      A = new int[array.length];
+      System.arraycopy(array, 0, A, 0, array.length); 
+
+      p = lower;
+      r = upper;
+   }
+
+   public int partition(int p, int r) {
       int x = A[r];
       int i = p - 1;
       for (int j = p; j < r; j++) {
          if (A[j] <= x) {
             i = i + 1;
+            swap(i, j);
+         }
+      }
+      swap(i+1, r);
+      return i+1;
+   }
+
+   /* sort into nonincreasing order */
+   public int partition2(int p, int r) {
+      int x = A[r];
+
+      int i = p - 1;
+      for (int j = p; j < r; j++) {
+         if (A[j] > x) {
+            i = i + 1;
+            swap(i, j);
+         }
+      }
+      swap(i+1, r);
+      return i+1;
+
+   }
+
+   public void swap(int i, int j) {
+      if  ((i >= p) && (i <= r) && (j >= p) && (j <= r)) {
+         int temp = A[i];
+         A[i] = A[j];
+         A[j] = temp;
+      }
+   }
+
+   public void qSort(int lower, int upper) {
+      if (lower < upper) {
+         int q = partition(lower, upper);
+
+         qSort(lower, q-1);
+         qSort(q+1, upper);
+      }
+   }
+
+   void showArray() {
+      for (int i = 0; i < A.length; i++) {
+         if (i+1 == A.length) {
+            System.out.println(A[i]);
+         } else {
+            System.out.print( A[i] + ", ");
          }
       }
    }
 
-   public void qSort(A, p, r) {
-      if (p < r) {
-         q = partition(A, p, r);
-         qSort(A, p, q-1);
-         qSort(A, q+1, r);
-      }
+   public static void main(String[] args) {
+      //int[] array = new int[]{2, 8, 7, 1, 3, 5, 6, 4};
+      int[] array = new int[]{2, 8, 7, 4, 3, 5, 6, 1};
+
+      QuickSort qs = new QuickSort(array, 0, array.length-1);
+      
+      qs.qSort(0, array.length-1);
+
+      qs.showArray();
+
+      return;
    }
 }
 
