3ad1444beabc799669fb0db390bcf3f8e2fe6c20
alexyu <alexyu@alexyu-ThinkPad-X230.(none)>
2013-05-04 23:53:11 -0700

    git commit -a

--- OODesign/Chapter1/Duck.java
+++ OODesign/Chapter1/Duck.java
@@ -1,83 +1,90 @@
 interface FlyBehavior {
-   public:
-   void fly();
+   public void fly();
 }
 
 class FlyWithWings implements FlyBehavior {
-   public:
-   void fly() {
+   public void fly() {
       System.out.println("I am flying...");
    }
 }
 
 class FlyNoWay implements FlyBehavior {
-   public:
-   void fly() {
+   public void fly() {
       System.out.println("I cannot fly.");
    }
 }
 
 interface QuackBehavior{
-   public:
-   void quack();
+   public void quack();
 }
 
 class Quack implements QuackBehavior{
-   public:
-   void quack() {
+   public void quack() {
       System.out.println("Quack");
    }
 }
 
 class MuteQuack implements QuackBehavior{
-   public:
-   void quack() {
+   public void quack() {
       System.out.println("<< Silence >>");
    }
 }
 
 class Squeak implements QuackBehavior{
-   public:
-   void quack() {
+   public void quack() {
       System.out.println("Squeak");
    }
 }
 
 
-class Duck {
-   public:
-   FlyBehavior flyBehavior;
-   QuackBehavior quackBehavior;
+abstract class Duck {
+   public void swim() {
+      System.out.println("Every duck can swim. Even decoy!");
+   }
+   
+   public abstract void display();
 
-   void swim();
-   void display();
-   void PerformQuack();
-   void PerformFly();
-   void setFlyBehavior();
-   void setQuackBehavior();
+   void PerformQuack() {
+      quackBehavior.quack();     
+   }
+   
+   void PerformFly() {
+      flyBehavior.fly();
+   }
+
+   void setFlyBehavior(FlyBehavior fb) {
+      flyBehavior = fb;
+   }
+   
+   void setQuackBehavior(QuackBehavior qb) {
+      quackBehavior = qb;
+   }
+
+   private FlyBehavior flyBehavior;
+   private QuackBehavior quackBehavior;
 }
 
 class MallardDuck extends Duck {
-   void display {
+   public void display() {
       System.out.println("I am a MallarDuck!");
    }
 }
 
 class RedHeadDuck extends Duck {
-   void display {
+   public void display() {
       System.out.println("I am a RedHeadDuck!");
    }
 }
 
 
 class RubberDuck extends Duck {
-   void display {
+   public void display() {
       System.out.println("I am a RubberDuck!");
    }
 }
 
 class DecoyDuck extends Duck {
-   void display {
+   public void display() {
       System.out.println("I am a DecoyDuck!");
    }
 }
