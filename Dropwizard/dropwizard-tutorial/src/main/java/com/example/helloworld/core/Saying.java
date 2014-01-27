fa920e316f4590b2fb9089da1990829b4a95f42c
alexyu <alexyu@alexyu-ThinkPad-X230.(none)>
2014-01-26 21:48:55 -0800

    Add hibernate-tutorial and dropwizard-tutorial

---
+++ Dropwizard/dropwizard-tutorial/src/main/java/com/example/helloworld/core/Saying.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+/**
+ * @author alexyu
+ *
+ */
+package com.example.helloworld.core;
+
+public class Saying {
+	private final long id;
+	private final String content;
+	
+	public Saying(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+}
