65cf7d874e3ecbb4e9d515f152dae88448ca134b
Alex Yu <ayu@alexyu.disternetdomain.com>
2014-04-11 17:52:01 -0700

    git commit -a

--- C/note.c
+++ C/note.c
@@ -125,8 +125,6 @@
 
     putValue<int> (fd, freq)
 
-
-
 -   Interesting...(??):
 
     XMLPUBFUN xmlChar * XMLCALL	
@@ -136,3 +134,82 @@
 
     Note: #define XMLPUBFUN
           #define XMLCALL
+
+-   Difference between global variables and static variable:
+
+    There are two separate concepts here:
+
+    -   scope, which determines where a name can be accessed, and
+    -   storage duration, which determines when a variable is created and destroyed.
+
+    Global variables (pedantically, variables with file scope (in C) or namespace scope (in C++)) are accessible at any point after their declaration:
+
+    Static variables 
+
+    Static variables (pedantically, variables with static storage duration) have a lifetime that lasts until the end of the program. If they are local 
+    variables, then their value persists when execution leaves their scope.
+
+    for (int i = 0; i < 5; ++i) {
+        static int n = 0;
+        printf("%d ", ++n);  // prints 1 2 3 4 5  - the value persists
+    }
+
+    Note that the static keyword has various meanings apart from static storage duration. On a global variable or function, it gives it internal linkage 
+    so that it's not accessible from other translation units; on a C++ class member, it means there's one instance per class rather than one per object. 
+    Also, in C++ the auto keyword no longer means automatic storage duration; it now means automatic type, deduced from the variable's initialiser.
+
+-   How to use one line to test whether a num is pow of 2 or not
+
+    if((num != 1) && (num & (num - 1))) { /* make num pow of 2 */ }
+
+-   A union is a special data type available in C that enables you to store different data types in the same memory location. You can define a 
+    union with many members, but only one member can contain a value at any given time. Unions provide an efficient way of using the same memory 
+    location for multi-purpose.
+
+    E.g.
+
+    union Data
+    {
+       int i;
+       float f;
+       char  str[20];
+    } data;
+
+    Now, a variable of Data type can store an integer, a floating-point number, or a string of characters. This means that a single variable ie. same 
+    memory location can be used to store multiple types of data. You can use any built-in or user defined data types inside a union based on your requirement.
+
+    The memory occupied by a union will be large enough to hold the largest member of the union. For example, in above example Data type will occupy 20 
+    bytes of memory space because this is the maximum space which can be occupied by character string. Following is the example which will display total memory 
+    size occupied by the above union:
+
+    #include <stdio.h>
+    #include <string.h>
+     
+    union Data
+    {
+       int i;
+       float f;
+       char  str[20];
+    };
+     
+    int main( )
+    {
+       union Data data;        
+
+       data.i = 10;
+       data.f = 220.5;
+       strcpy( data.str, "C Programming");
+
+       printf( "data.i : %d\n", data.i);
+       printf( "data.f : %f\n", data.f);
+       printf( "data.str : %s\n", data.str);
+
+       return 0;
+    }
+
+    data.i : 1917853763
+    data.f : 4122360580327794860452759994368.000000
+    data.str : C Programming
+
+    Here, we can see that values of i and f members of union got corrupted because final value assigned to the variable has occupied the memory location and 
+    this is the reason that the value if str member is getting printed very well.
