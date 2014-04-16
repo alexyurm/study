ce4934c9a8b10223cd811484f525249abe83209c
alexyu <alexyu@alexyu-ThinkPad-X230.(none)>
2014-04-15 23:36:15 -0700

    git commit -a

--- DataStructure/EnglishReference/Introduction_to_Algorithms/Chapter6/MaxQueue.java
+++ DataStructure/EnglishReference/Introduction_to_Algorithms/Chapter6/MaxQueue.java
@@ -33,9 +33,10 @@ class max_p_queue {//max-priority queue
    //Constructor
    public max_p_queue() {
       heapSize = n = 0;
+      A = new double[100]; //give 100 double type space
    }
 
-   //Some useful methods
+   //left child
    public int left(int i) {
       //assert i < n/2 : "Position has no left child"; //?? why i < n/2
       if ( i < heapSize/2) {
@@ -45,31 +46,117 @@ class max_p_queue {//max-priority queue
       }
    }
 
+   //right child
+   public int right(int i) {
+      //assert i < (n-1)/2 : "Position has no right child"; //?? why i < (n-1)/2
+      if (i < (heapSize-1)/2) {
+         return i*2+2;
+      } else {
+         return -1;
+      }
+   }
+
+   //parent
+   public static int parent(int i) {
+      //return (int)(Math.ceil((double)i/2))-1;
+      //assert i > 0 : "Position has no parent";
+      if (i > 0) {
+         return (i-1)/2;
+      } else {
+         return -1;
+      }
+   }
+
+   //max-heapify, the parent is always larger than its children therefore the root stores the biggest element(it takes log(n) computation time).
+   void maxheapify(int i) {
+         int l = left(i);
+         int r = right(i);
+         int largest = i;
+      
+         //Find the largest elements among: me, left and right.
+         if ( l != -1) {
+            if(l <= heapSize && A[l] > A[i]) {
+               largest = l;
+            } else {
+               largest = i;
+            }
+         }
+
+         if (r != -1) {
+            if(r <= heapSize && A[r] > A[largest]) {
+               largest = r;
+            }
+         }
+
+         if (largest != i) {
+            double temp = A[largest];
+            A[largest] = A[i];
+            A[i] = temp;
+            maxheapify(largest); //Recurssion occurs here. Remember "largest" is the index.
+         }
+   }
 
    //Returns the element of S with the largest key
-   public int heapMax(int[] A) {
+   public double heapMax() {
       return A[0];
    }
    
    //Extract the largest element and returns
-   public int heapExtractMax(int[] A)
+   public double heapExtractMax()
    {
-      if (A.heapSize < 1) {
+      if (heapSize < 1) {
          System.out.println("heap underflow");
       }
 
-      int max = A[0];
-      A[0] = A[A.heapSize-1];
-      A.heapSize = A.heapSize - 1;
-
-      return 1;
-        
+      double max = A[0];
+      A[0] = A[heapSize-1]; //exchange the last with the first element in the heap
+      heapSize = heapSize - 1;
+      maxheapify(0); //heapify again
       
-   
-      //return A[]
+      return max;
+   }
+
+   //Increase the key value of ith element(it takes log(n) computation time)
+   public boolean increaseKey(double key, int i) {
+      if (key < A[i]) {
+         System.out.println("new key is smaller than current key");
+         return false;
+      } else {
+         A[i] = key;
+         while ((i > 0) && (A[parent(i)] < A[i])) {
+            double temp = A[i];
+            A[i] = A[parent(i)];
+            A[parent(i)] = temp;
+            i = parent(i);
+         }
+         return true;
+      }
+   }
+
+   //Insert an element into the heap
+   public void insert(double key) {
+      n++;
+      heapSize++;
+      
+      A[heapSize-1] = Double.NEGATIVE_INFINITY;
+      increaseKey(key, heapSize-1);
    }
 
    private int heapSize; //the size of the heap: heapSize <= n
-   private int A[]; //the array
+   private double A[]; //the array
    private int n; //the array size: heapSize <= n
+
+   public static void main(String[] args) {
+      max_p_queue mpq = new max_p_queue();
+      mpq.insert(1.5);
+      mpq.insert(20);
+      mpq.insert(-2);
+      mpq.insert(4);
+      
+      mpq.heapExtractMax();
+      mpq.heapExtractMax();
+      System.out.println(mpq.heapMax());
+
+      return; 
+   }
 }
